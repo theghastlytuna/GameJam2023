99c9701b33a0d2252b81920cabbdb246bf3898fd
AIdanpdn <144922298+AIdanpdn@users.noreply.github.com>
2023-10-21 17:48:19 -0400

    Walls

---
+++ Assets/Environment/Eviornmental Scripts/Wall.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    private bool Broken = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Broken = true;
+        if (Broken)
+        {
+            if (collision.gameObject.tag == "Limb")
+            {
+                gameObject.GetComponent<Collider2D>().enabled = false;
+            }
+        }
+    }
+
+    public void DropWall()
+    {
+        gameObject.GetComponent<Collider2D>().enabled = false;
+        Debug.Log("WallDropped");
+    }
+}
