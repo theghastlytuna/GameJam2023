886d4914c065fe882665490cf2414b690362759b
HaytharAli <Haiderali360@gmail.com>
2023-10-21 22:38:42 -0400

    Pick up arms working

--- Assets/Scripts/Limbs/LimbBase.cs
+++ Assets/Scripts/Limbs/LimbBase.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 [System.Serializable]
 public class LimbBase : MonoBehaviour
 {
+    [SerializeField] bool Debug_Printing = false;
+
     public LimbSlot limbType;
     [SerializeField] bool Grabbable = true;
     [SerializeField] bool AttachedToBody;
 
+    CircleCollider2D circleCollider;
     Rigidbody2D _rb;
 
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        circleCollider = GetComponent<CircleCollider2D>();
     }
 
     public virtual void Throw(Vector3 forceDir)
@@ -25,10 +29,22 @@ public class LimbBase : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (Debug_Printing)
+        {
+            Debug.Log("Collided");
+        }
         if (Grabbable && collision.gameObject.GetComponent<PlayerScript>())
         {
+            if (Debug_Printing)
+            {
+                Debug.Log("Grabbable and hit Player");
+            }
             if (collision.gameObject.GetComponent<PlayerScript>().SlotsOpen(((int)limbType)))
             {
+                if (Debug_Printing)
+                {
+                    Debug.Log("Player has available slots");
+                }
                 AttachToBody(collision.gameObject.GetComponent<PlayerScript>());
             }
         }
@@ -39,6 +55,7 @@ public class LimbBase : MonoBehaviour
         AttachedToBody = true;
         player.AttachLimb(this);
         Grabbable = false;
+        circleCollider.excludeLayers = LayerMask.GetMask("Player");
     }
 
     private void Update()
